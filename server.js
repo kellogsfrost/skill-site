bc8d7a2ebf5c625aa180f082f53c8d28e9fe0f10
Hayden Schultz <hayden@Haydens-MacBook-Pro.local>
2019-06-18 09:37:05 -0700

    Done

--- server.js
+++ server.js
@@ -5,9 +5,36 @@ const layouts = require('express-ejs-layouts');
 app.set("view engine", "ejs");
 app.use(layouts);
 
+var skills = [
+    "JavaScript",
+    "HTML 5",
+    "CSS 3",
+    "Node JS",
+    "Express JS",
+    "Crooning"
+]
+
+var contact = {
+    name: "Steve Peters",
+    email: "steve@steve.com",
+    phone: "206-555-1212",
+    fax: "Who uses faxes anymore?",
+    site: "http://steve.io"
+};
+
 app.get('/', function(req, res){
-    res.send("yeah it works");
+    res.render('index');
 });
+
+app.get('/skills', function(req, res){
+    res.render('skill', {skills});
+});
+
+app.get('/contacts', function(req, res){
+    res.render('contact', {contact});
+});
+
+
 
 app.listen(3000, function(){
     console.log("Listening on port 3000...😈");
